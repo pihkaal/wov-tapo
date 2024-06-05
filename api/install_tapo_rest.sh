accd271157a1ce1895ffd66ca2ae098468e61398
Pihkaal <pihkaal@proton.me>
2024-06-05 13:08:40 +0200

    feat(api): install and run script

---
+++ api/install_tapo_rest.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/sh
+
+# Install bin
+
+pushd ~/.cache
+
+echo "> Install"
+
+if [ ! -d "tapo-rest" ]; then
+  git clone https://github.com/ClementNerma/tapo-rest
+fi
+
+cd ./tapo-rest
+git pull
+
+cargo build --release
+sudo cp ./target/release/tapo-rest /usr/local/bin/
+
+popd
+
+
+# Configure
+
+echo "> Configure"
+
+if [ ! -f ~/.config/tapo-rest/config.json ]; then
+  mkdir -p ~/.config/tapo-rest
+
+  echo "Tapo username: "
+  read tapo_username
+
+  echo "Tapo password: "
+  read tapo_password
+
+  echo "Device name: "
+  read device_name
+
+  echo "Device type: "
+  read device_type
+
+  echo "Device IP: "
+  read device_ip
+
+  config_template=$(cat ./config.json.template)
+  config_template="${config_template/TAPO_USERNAME/${tapo_username}}"
+  config_template="${config_template/TAPO_PASSWORD/${tapo_password}}"
+  config_template="${config_template/DEVICE_NAME/${device_name}}"
+  config_template="${config_template/DEVICE_TYPE/${device_type}}"
+  config_template="${config_template/DEVICE_IP/${device_ip}}"
+
+  echo "$config_template" > ~/.config/tapo-rest/config.json
+fi
+
+# Setup deamon
+
+echo "> Service setup"
+
+auth_password=$(openssl rand -base64 32)
+
+service_template=$(cat ./tapo_rest.service.template)
+service_template="${service_template/AUTH_PASSWORD/${auth_password}}"
+
+echo "$service_template" | sudo tee /etc/systemd/system/tapo_rest.service > /dev/null
+
+sudo systemctl daemon-reload
+sudo systemctl enable tapo_rest.service
+sudo systemctl start tapo_rest.service
+
+echo "> Done"
+
+echo "PORT: 6670"
+echo "PASSWORD: $auth_password"
